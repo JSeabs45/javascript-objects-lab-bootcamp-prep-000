8f5f126239caf5e6b522606c16b4c18d09d3519b
Joshua Seabolt <jseabolt45@hotmail.com>
2018-10-16 17:09:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,8 +2,13 @@ var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign({}, object, { [key]: value })
-}
-
+ }
+ 
+ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
+  object[key] = value;
+  return object;
+ }
+ 
  function deleteFromObjectByKey(object, key) {
   var newObj = Object.assign({}, object);
   delete newObj.key;
@@ -11,12 +16,17 @@ function updateObjectWithKeyAndValue(object, key, value) {
 }
 
  function destructivelyDeleteFromObjectByKey(object, key) {
-  delete object.key;
-  return object;
+ delete object.key;
+ return object;
 }
 
- function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
-  object[key] = value;
+ function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, object);
+  delete newObj[key];
+  return newObj;
+ }
+ 
+ function destructivelyDeleteFromObjectByKey(object, key){
+  delete object[key];
   return object;
 }
-
